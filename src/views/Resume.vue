4c6886adb55a6bbc82cf4f76bd0a5505bc54bf78
vipul43 <saifunny43@gmail.com>
2021-07-06 00:10:39 +0530

    added mulitple pages viewing

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,6 +1,28 @@
 <template>
-  <embed
-    src="https://vipul43.sirv.com/vipulsabout/vipul_core_cv.pdf"
-    height="100%"
-  />
+  <div>
+    <pdf v-for="i in numPages" :key="i" :src="src" :page="i"></pdf>
+  </div>
 </template>
+
+<script>
+import pdf from "vue-pdf";
+var loadingTask = pdf.createLoadingTask(
+  "https://vipul43.sirv.com/vipulsabout/vipul_core_cv.pdf"
+);
+
+export default {
+  name: "Resume",
+  components: {
+    pdf,
+  },
+  data: () => ({
+    src: loadingTask,
+    numPages: 0,
+  }),
+  mounted() {
+    this.src.promise.then((pdf) => {
+      this.numPages = pdf.numPages;
+    });
+  },
+};
+</script>
