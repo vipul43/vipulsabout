52deda7003bae60fe27162f523cac460b1eaf8fe
vipul43 <saifunny43@gmail.com>
2021-07-06 01:12:06 +0530

    added loading animation to resume page

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container v-if="show">
     <v-btn
       class="ma-2"
       outlined
@@ -9,6 +9,14 @@
       Download PDF
     </v-btn>
     <pdf v-for="p in numPages" :key="p" :src="src" :page="p"></pdf>
+  </v-container>
+  <v-container v-else>
+    <v-progress-circular
+      indeterminate
+      :size="70"
+      :width="7"
+      color="light-black"
+    ></v-progress-circular>
   </v-container>
 </template>
 
@@ -21,6 +29,7 @@ export default {
     pdf,
   },
   data: () => ({
+    show: false,
     src: undefined,
     numPages: 0,
   }),
@@ -31,6 +40,9 @@ export default {
     this.src.promise.then((pdf) => {
       this.numPages = pdf.numPages;
     });
+    setTimeout(() => {
+      this.show = true;
+    }, 2000);
   },
 };
 </script>
