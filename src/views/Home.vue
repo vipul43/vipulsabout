ece0243ebf1096ab6381642171e747e6990ecafb
vipul43 <saifunny43@gmail.com>
2021-09-19 01:09:57 +0530

    formatter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,34 +6,13 @@
           "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola
           imperdiet nec ut, sed euismod convenire principes at. Est et nobis
           iisque percipit, an vim zril disputando voluptatibus, vix an salutandi
-          sententiae."</v-card-text
-        ></v-col
-      >
+          sententiae."
+        </v-card-text>
+      </v-col>
       <v-col>
         <a href="https://github.com/vipul43">
           <v-img
             src="https://github-readme-stats.vercel.app/api?username=vipul43&show_icons=true&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=272727"
-            max-width="492"
-            max-height="193"
-          ></v-img>
-        </a>
-        <a href="https://github.com/vipul43/FACE_MASK_DETECTION">
-          <v-img
-            src="https://github-readme-stats.vercel.app/api/pin/?username=pulavartivinay&repo=FACE_MASK_DETECTION&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
-            max-width="492"
-            max-height="193"
-          ></v-img>
-        </a>
-        <a href="https://github.com/vipul43/CS5007_DL">
-          <v-img
-            src="https://github-readme-stats.vercel.app/api/pin/?username=vipul43&repo=CS5007_DL&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
-            max-width="492"
-            max-height="193"
-          ></v-img>
-        </a>
-        <a href="https://github.com/vipul43/CS2180_AI_LAB">
-          <v-img
-            src="https://github-readme-stats.vercel.app/api/pin/?username=vipul43&repo=CS2180_AI_LAB&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
             max-width="492"
             max-height="193"
           ></v-img>
