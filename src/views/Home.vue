9256836f340316a84170af50f24e60ca5e25b438
vipul43 <saifunny43@gmail.com>
2021-09-18 23:18:15 +0530

    added more github info cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,11 +10,34 @@
         ></v-col
       >
       <v-col>
-        <v-img
-          src="https://github-readme-stats.vercel.app/api?username=vipul43&show_icons=true&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=272727"
-          max-width="492"
-          max-height="193"
-        ></v-img>
+        <a href="https://github.com/vipul43">
+          <v-img
+            src="https://github-readme-stats.vercel.app/api?username=vipul43&show_icons=true&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=272727"
+            max-width="492"
+            max-height="193"
+          ></v-img>
+        </a>
+        <a href="https://github.com/vipul43/FACE_MASK_DETECTION">
+          <v-img
+            src="https://github-readme-stats.vercel.app/api/pin/?username=pulavartivinay&repo=FACE_MASK_DETECTION&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
+            max-width="492"
+            max-height="193"
+          ></v-img>
+        </a>
+        <a href="https://github.com/vipul43/CS5007_DL">
+          <v-img
+            src="https://github-readme-stats.vercel.app/api/pin/?username=vipul43&repo=CS5007_DL&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
+            max-width="492"
+            max-height="193"
+          ></v-img>
+        </a>
+        <a href="https://github.com/vipul43/CS2180_AI_LAB">
+          <v-img
+            src="https://github-readme-stats.vercel.app/api/pin/?username=vipul43&repo=CS2180_AI_LAB&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
+            max-width="492"
+            max-height="193"
+          ></v-img>
+        </a>
       </v-col>
     </v-row>
   </v-container>
