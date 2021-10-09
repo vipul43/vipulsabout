f19521ecce32a1253f322c10800d57b1f724e429
vipul43 <saifunny43@gmail.com>
2021-10-09 23:33:08 +0530

    added rich text editors to blog pages

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 import router from './router'
+import CKEditor from '@ckeditor/ckeditor5-vue2';
 
 Vue.config.productionTip = false
+Vue.use(CKEditor);
 
 new Vue({
   vuetify,
--- src/views/Blogs/LifeBlogs/Perseverance.vue
+++ src/views/Blogs/LifeBlogs/Perseverance.vue
@@ -1,8 +1,29 @@
 <template>
   <v-container class="blog-background">
     <div class="font-weight-light text-h2 text-center">Perseverance</div>
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
   </v-container>
 </template>
+
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+export default {
+  name: "DeepLearning",
+  data: () => ({
+    admin: true,
+    editor: ClassicEditor,
+    editorData: "",
+    editorConfig: {
+      // The configuration of the editor.
+    },
+  }),
+};
+</script>
 
 <style>
 .blog-background {
