e21d9d2f968c85efb5bd3087af18b1f8fa38a0b5
vipul43 <saifunny43@gmail.com>
2021-12-29 16:57:05 +0530

    added firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.FIREBASE_WEB_API_KEY,
     authDomain: "vipulsabout.firebaseapp.com",
     projectId: "vipulsabout",
     storageBucket: "vipulsabout.appspot.com",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.FIREBASE_PROJECT_NUMBER,
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
